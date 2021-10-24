4f3c064107ee97eff5947237145d64c71c60e7c3
Documenter.jl <documenter@juliadocs.github.io>
2021-10-24 02:56:25 +0000

    build based on bf1c6c2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PrettyLinearAlgebra","category":"page"},{"location":"#PrettyLinearAlgebra","page":"Home","title":"PrettyLinearAlgebra","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PrettyLinearAlgebra.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PrettyLinearAlgebra]","category":"page"}]
+}
