a4d3a5bc25baaad44df6d2fa566562521a71b06b
Ryan S. Videgar-Laird <Ryan.Videgar-Laird@tuta.io>
2021-10-23 23:13:41 -0400

    basic unit test for matex()

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using PrettyLinearAlgebra
 using Test
+using LinearAlgebra
 
 @testset "PrettyLinearAlgebra.jl" begin
-    # Write your tests here.
+    v1 = [1, 5, 9, 99]
+    m1 = [1 5 9; 4 3 1; 33 44 55]
+    
+    @test matex(v1) == "\\begin{bmatrix} 1 \\\\ 5 \\\\ 9 \\\\ 99\\end{bmatrix}"
+    @test matex(m1) == "\\begin{bmatrix}1 & 5 & 9 \\\\ 4 & 3 & 1 \\\\ 33 & 44 & 55\\end{bmatrix}"
 end
